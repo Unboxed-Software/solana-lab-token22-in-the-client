f4ef01e49f521afbc8db1e10416761262cc75f27
pbillingsby <45179070+PBillingsby@users.noreply.github.com>
2024-03-29 12:12:49 -0400

    change main function to iife

--- src/fetch-token-info.ts
+++ src/fetch-token-info.ts
@@ -1,5 +1,5 @@
 import { AccountLayout, getMint } from "@solana/spl-token"
-import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
+import { Connection, PublicKey } from "@solana/web3.js"
 
 export type TokenTypeForDisplay = 'Token Program' | 'Token Extension Program';
 
@@ -17,39 +17,39 @@ export async function fetchTokenInfo(
   programId: PublicKey,
   type: TokenTypeForDisplay
 ): Promise<TokenInfoForDisplay[]> {
-	const tokenAccounts = await connection.getTokenAccountsByOwner(
-		owner,
-		{programId}
-	)
+  const tokenAccounts = await connection.getTokenAccountsByOwner(
+    owner,
+    { programId }
+  )
 
-    const ownedTokens: TokenInfoForDisplay[] = []
+  const ownedTokens: TokenInfoForDisplay[] = []
 
-    for (const tokenAccount of tokenAccounts.value) {
-        const accountData = AccountLayout.decode(tokenAccount.account.data)
+  for (const tokenAccount of tokenAccounts.value) {
+    const accountData = AccountLayout.decode(tokenAccount.account.data)
 
-        const mintInfo = await getMint(connection, accountData.mint, 'finalized', programId)
+    const mintInfo = await getMint(connection, accountData.mint, 'finalized', programId)
 
-        ownedTokens.push({
-			mint: accountData.mint,
-            amount: Number(accountData.amount),
-            decimals: mintInfo.decimals,
-			displayAmount: Number(accountData.amount) / (10**mintInfo.decimals),
-			type,
-		})
-    }
+    ownedTokens.push({
+      mint: accountData.mint,
+      amount: Number(accountData.amount),
+      decimals: mintInfo.decimals,
+      displayAmount: Number(accountData.amount) / (10 ** mintInfo.decimals),
+      type,
+    })
+  }
 
   return ownedTokens;
 }
 
 export async function fetchTokenProgramFromAccount(
-    connection: Connection,
-    mint: PublicKey
-  ){
-    //Find the program ID from the mint
-    const accountInfo = await connection.getParsedAccountInfo(mint);
-    if (accountInfo.value === null) {
-        throw new Error('Account not found');
-    }
-    const programId = accountInfo.value.owner;
-    return programId;
+  connection: Connection,
+  mint: PublicKey
+) {
+  //Find the program ID from the mint
+  const accountInfo = await connection.getParsedAccountInfo(mint);
+  if (accountInfo.value === null) {
+    throw new Error('Account not found');
   }
+  const programId = accountInfo.value.owner;
+  return programId;
+}
